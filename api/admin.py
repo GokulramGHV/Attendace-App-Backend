4244f383257f9de350c8c02835bc8d4f7da0140e
Gokulram A <gokulram2010418@ssn.edu.in>
2022-10-06 06:53:41 +0530

    first commit

---
+++ api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Attendance)
+admin.site.register(TimeTable)
---
+++ api/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+DAYS_OF_WEEK = (
+    (0, "Monday"),
+    (1, "Tuesday"),
+    (2, "Wednesday"),
+    (3, "Thursday"),
+    (4, "Friday"),
+    (5, "Saturday"),
+    (6, "Sunday"),
+)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=100)
+    reg_no = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    attendance_percentage = models.DecimalField(max_digits=3, decimal_places=2)
+    course_enrolled = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class Attendance(models.Model):
+    session = models.DateTimeField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class TimeTable(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    day = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
